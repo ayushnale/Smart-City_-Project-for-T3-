cabb9ed784061864fbe52c9cc5368de02f7d4d95
ayushnale <you@example.com>
2025-09-15 14:46:14 +0530

    api respond and  error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,21 @@
 // require ('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
+import express from "express";
 import connectDB from "./db/index.js";
+
+const app = express()
 
 dotenv.config({
     path: './.env'
 })
-connectDB();
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,() => {
+        console.log(`server running on PORT :${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(`MONGODB Connection failed!!!\n`, err)
+})
